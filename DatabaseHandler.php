ae8333b1e9ed9baa6b8bd2c022e06f61dd123c5e
Bence Ladoczki <info@ldczk.com>
2020-09-30 21:12:07 +0200

    second add

--- DatabaseHandler.php
+++ DatabaseHandler.php
@@ -128,9 +128,10 @@ class DatabaseHandler {
     }
     public function delete_entry_from_table($table_name,$field,$value) {
         try{
-        $stmt = $this->connector->db->prepare("DELETE FROM `".$table_name."` WHERE ".$field." = \"".$value."\" ");
+        $query = "DELETE FROM `".$table_name."` WHERE ".$field." = \"".$value."\" ";
+        $stmt = $this->connector->db->prepare($query);
         $stmt->execute();
-        return "OK";
+        return $query;
         }
         catch(PDOException $e)
         {
