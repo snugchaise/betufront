5fa9d82ff02365c8a21b30956db8431ab5e04d4c
Bence Ladoczki <info@ldczk.com>
2020-09-30 14:57:11 +0200

    second add

--- index.php
+++ index.php
@@ -2068,6 +2068,7 @@ switch ($request) {
       echo '<div>'.PHP_EOL;
       echo '<h3>'.$bfs['book_author'].'</h3>'.PHP_EOL;
       echo '<h4>'.$bfs['book_title'].'</h4>'.PHP_EOL;
+      echo '<h5>'.$bfs['book_publisher'].' '.'['.$bfs['book_publish_date'].']</h5>'.PHP_EOL;
       echo '</div>'.PHP_EOL;
      }
      echo '</div>'.PHP_EOL;
